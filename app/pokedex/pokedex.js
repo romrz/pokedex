99085c5fc196e0c856368040d7ec0a4e3577542e
Romario <rom23r@gmail.com>
2017-04-17 13:04:03 -0500

    All functionality done. Ready to refactor and stylize.

--- app/pokedex/pokedex.js
+++ app/pokedex/pokedex.js
@@ -6,16 +6,20 @@ app.directive('pokedex', function() {
 		controller: ['$http', function($http) {
 			this.pokemons = [];
 			this.pokemon = null;
+			this.loading = false;
 
 			this.url = 'https://pokeapi.co/api/v2/pokemon?limit=21';
 
 			var pokedex = this;
 
 			this.getPokemons = function() {
+				pokedex.loading = true;
+
 				$http.get(pokedex.url).then(
 					function(response) {
 						pokedex.pokemons = pokedex.pokemons.concat(response.data.results);
 						pokedex.url = response.data.next;
+						pokedex.loading = false;
 					},
 					function(error) {
 						console.log(error);
